266d70bc1687e2ede0ac4f7af79ace806016b9ba
bnesiba <bnesiba@gmail.com>
2026-03-11 17:30:48 -0400

    wip: reducer implementation

--- graphFlow/graphFlow/Reducer.cs
+++ graphFlow/graphFlow/Reducer.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ActionFlow.Models;
+using graphFlow.models;
 
 namespace graphFlow
 {
-    public class Reducer
+    public class Reducer<T> : IFlowStateReducer<GraphState<T>>
     {
+        public GraphState<T> InitialState => new GraphState<T>();
+
+        public List<IFlowReductionBase<GraphState<T>>> Reductions => new List<IFlowReductionBase<GraphState<T>>> 
+        {
+            this.reduce(nodesAndEdgesRun_OnNodeComplete_AddNodesAndEdgesRun, Actions.NodeExecuted<T>()),
+            this.reduce(nodesAndEdgesRun_OnEdgeEvaluation_AddNodesAndEdgesRun, Actions.EdgeEvaluation<T>()),
+            this.reduce(StateObject_OnNodeComplete_UpdateStateObject, Actions.NodeExecuted<T>())
+        };
+
+        //Reducer Methods
+        public GraphState<T> nodesAndEdgesRun_OnNodeComplete_AddNodesAndEdgesRun(FlowAction<GraphNodeResult<T>> nodeExecutedAction, GraphState<T> currentState)
+        {
+            currentState.nodesAndEdgesRun.Add(nodeExecutedAction.Parameters.nodeExecuted.name);
+            return currentState;
+        }
+        public GraphState<T> nodesAndEdgesRun_OnEdgeEvaluation_AddNodesAndEdgesRun(FlowAction<GraphEdge<T>> edgeEvaluatedAction, GraphState<T> currentState)
+        {
+            currentState.nodesAndEdgesRun.Add(edgeEvaluatedAction.Parameters.name);
+            return currentState;
+        }
+
+        //TODO: implement properly. This will blow up on partials, which we don't want.
+        public GraphState<T> StateObject_OnNodeComplete_UpdateStateObject(FlowAction<GraphNodeResult<T>> nodeExecutedAction, GraphState<T> currentState)
+        {
+            var stateObj = nodeExecutedAction.Parameters.nodeOutput;
+            currentState.stateObject = stateObj;
+            return currentState;
+        }
     }
 }
--- graphFlow/graphFlow/models/GraphState.cs
+++ graphFlow/graphFlow/models/GraphState.cs
@@ -8,16 +8,34 @@ namespace graphFlow.models
 {
     public class GraphState<T> : GraphStateBase
     {
+        public GraphState(T state) : base()
+        {
+            stateObject = state;
+        }
+
+        public GraphState(): base()
+        {
+            stateObject = default(T);
+        }
+
         public T stateObject {  get; set; }
+
     }
 
     public class GraphState : GraphStateBase
     {
+        public GraphState() : base() { }
     }
 
     public abstract class GraphStateBase
     {
         public Guid id {  get; set; }
         public List<string> nodesAndEdgesRun { get; set; }
+
+        public GraphStateBase()
+        {
+            id = Guid.NewGuid();
+            nodesAndEdgesRun = new List<string>();
+        }
     }
 }
